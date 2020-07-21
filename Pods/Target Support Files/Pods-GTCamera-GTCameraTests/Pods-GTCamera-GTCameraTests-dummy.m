4736d514de47f75f303ac06817cde1cbb6cd21b6
t.kazama <takeo@kazama.biz>
2020-07-21 22:51:15 +0900

    Carthage build test

---
+++ Pods/Target Support Files/Pods-GTCamera-GTCameraTests/Pods-GTCamera-GTCameraTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GTCamera_GTCameraTests : NSObject
+@end
+@implementation PodsDummy_Pods_GTCamera_GTCameraTests
+@end
---
+++ Pods/Target Support Files/Pods-GTCameraExample/Pods-GTCameraExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GTCameraExample : NSObject
+@end
+@implementation PodsDummy_Pods_GTCameraExample
+@end
